fbf498ac7d54409e4465bb06390976fcd7237609
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-02-13 11:38:27 -0800

    started my global variables and constructor in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Global Variables
+let totalClicks = 0;
+let clicksAllowed = 25;
+let allBusProducts = [];
+let imageOne = document.querySelector('section img:first-child');
+let imageTwo = document.querySelector('section img:nth-child(2)');
+let imageThree = document.querySelector('section img:last-child');
+
+// Constructor
+function BusProduct(name, fileExtension = 'jpg') {
+  this.name = name;
+  this.src = `img/${name}.${fileExtension}`;
+  this.views = 0;
+  this.clicks = 0;
+  allBusProducts.push(this);
+}
+
