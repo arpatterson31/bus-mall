4f19b11ac3913f9822223ca01d005140e376b0d1
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-02-16 21:02:25 -0800

    added my chart to my js file and completed my function

--- js/app.js
+++ js/app.js
@@ -44,14 +44,14 @@ new BusProduct('water-can');
 new BusProduct('wine-glass');
 
 
-function getRandomIndex(){
+function getRandomIndex() {
   return Math.floor(Math.random() * allBusProducts.length);
 }
 
-function renderBusProduct(){
+function renderBusProduct() {
   while (productIndexArray.length < uniqueNumberSelector) {
     let randomNumber = getRandomIndex();
-    while (!productIndexArray.includes(randomNumber)){
+    while (!productIndexArray.includes(randomNumber)) {
       productIndexArray.unshift(randomNumber);
     }
   }
@@ -73,50 +73,105 @@ function renderBusProduct(){
   allBusProducts[thirdProductIndex].views++;
 }
 
-function renderResults(){
+function renderResults() {
   let myList = document.querySelector('ul');
-  for (let i = 0; i < allBusProducts.length; i++){
+  for (let i = 0; i < allBusProducts.length; i++) {
     let li = document.createElement('li');
     li.textContent = `${allBusProducts[i].name} had ${allBusProducts[i].votes} votes and was seen ${allBusProducts[i].views} times`;
     myList.appendChild(li);
   }
 }
 
-function handleClick(event){
+function handleClick(event) {
   totalClicks++;
   let productClicked = event.target.title;
 
-  for (let i = 0; i < allBusProducts.length; i++){
-    if(productClicked === allBusProducts[i].name) {
+  for (let i = 0; i < allBusProducts.length; i++) {
+    if (productClicked === allBusProducts[i].name) {
       allBusProducts[i].votes++;
     }
   }
   renderBusProduct();
-  if (totalClicks === clicksAllowed){
+  if (totalClicks === clicksAllowed) {
     myContainer.removeEventListener('click', handleClick);
-    // renderBusChart();
-  }
-}
-
-function handleButtonClick(event){ //eslint-disable-line
-  if(totalClicks === clicksAllowed){
-    renderResults();
+    renderBusChart();
   }
 }
 
 renderBusProduct();
 
-function renderBusChart(){
+function renderBusChart() {
   let busProductNames = [];
   let busProductViews = [];
   let busProductVotes = [];
 
-  for (let i = 0; i < allBusProducts.length; i++){
+  for (let i = 0; i < allBusProducts.length; i++) {
     busProductNames.push(allBusProducts[i].name);
-    busProductViews.push(allBusProducts.views);
-    busProductVotes.push(allBusProducts.votes);
+    busProductViews.push(allBusProducts[i].views);
+    busProductVotes.push(allBusProducts[i].votes);
   }
 
+  let chartData = {
+    type: 'bar',
+    data: {
+      labels: busProductNames,
+      datasets: [{
+        label: '# of Votes',
+        data: busProductVotes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views',
+        data: busProductViews,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  }
+
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, chartData);
 }
+
 myContainer.addEventListener('click', handleClick);
-myButton.addEventListener('click', handleButtonClick);
+
